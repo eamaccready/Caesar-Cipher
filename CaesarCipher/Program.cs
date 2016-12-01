d994feb33c2a2408b8c513d775916abd23a430c8
Elizabeth MacCready (InConsulting) <v-elmacc@microsoft.com>
2016-12-01 12:05:27 -0800

    Initial commit.

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create string of English alphabet and new Cypher object.
+            string English = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string testString = "BABAC";
+            Cipher EnglishCipher = new Cipher(English);
+            EnglishCipher.offset = -1;
+            string encrypted = EnglishCipher.Encrypt(testString);
+            Console.WriteLine(encrypted);
+            Console.WriteLine(EnglishCipher.Decrypt(encrypted));
+            Console.ReadLine();
+
+            
+        }
+    }
+
+   public class Cipher
+    {
+        string falphabet;
+        private int foffset;
+        public int offset
+        {
+            get
+            {
+                return foffset;
+            }
+            set
+            {
+                foffset = value;
+                while (foffset < 0)
+                {
+                    foffset += falphabet.Length;
+                }
+            }
+        }
+        public Cipher(string alphabet)
+        {
+            falphabet = alphabet;
+        }
+
+        /* keep in case offset needs to become truly a method.
+       //method to set offset for ciphering 
+        public string OffsetBy(int offset)
+        {
+            string result = string.Empty;
+            char[] offsetabet = falphabet.ToCharArray();
+            while (offset < 0)
+            {
+                offset += offsetabet.Length;
+            }
+
+            for (int i = 0; i < offsetabet.Length; i++)
+            {
+                int shiftedIndex = (i + offset) % offsetabet.Length; 
+                result += offsetabet[shiftedIndex];
+            }  
+            return result; 
+        } */
+
+         //method to cipher a a string with the given alphabet and current ofset
+        public string Encrypt(string message)
+        {
+            string result = string.Empty;
+            char[] messageChars = message.ToCharArray();
+            for (int i=0; i < messageChars.Length; i++)
+            {
+                // Gets index of character[i] in the alphabet field.
+                int indexInAlphabet = falphabet.IndexOf(messageChars[i]);
+
+                // Shifts index by offset and gets resulting char
+                int shiftedIndex = (indexInAlphabet + offset) % falphabet.Length;
+                //append char to new string.
+                result += falphabet[shiftedIndex];
+            }
+            return result;
+        }
+
+       
+        //method to decipher the string with the given alphabet and current offset.
+        public string Decrypt(string encryptedMessage)
+        {
+            string result = string.Empty;
+            char[] messageChars = encryptedMessage.ToCharArray();
+            for (int i=0; i < messageChars.Length; i++)
+            {
+                int shiftedIndexInAlphabet = falphabet.IndexOf(messageChars[i]);
+                int trueIndex = (shiftedIndexInAlphabet - offset) % falphabet.Length;
+                //handels cases where index returns -.
+                if (trueIndex < 0)
+                {
+                    trueIndex = trueIndex * -1;
+                }
+                result += falphabet[trueIndex]; 
+            }
+            return result;
+        }
+    }
+}
---
+++ CipherTests/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CaesarCipher;
+
+namespace CipherTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       /* [TestMethod]
+        public void ZeroOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = 0;
+            Assert.AreEqual("ABC", c.OffsetBy(0));
+        }
+
+        [TestMethod]
+        public void NonZeroOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            Assert.AreEqual(c.OffsetBy(2), c.OffsetBy(5));
+            Assert.AreEqual("CAB", c.OffsetBy(5));
+        }
+
+        [TestMethod]
+        public void NegativeOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            Assert.AreEqual(c.OffsetBy(2), c.OffsetBy(-1));
+            Assert.AreEqual("CAB", c.OffsetBy(-1));
+        }
+        */
+
+        [TestMethod]
+        public void EncryptZeroOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = 0;
+            Assert.AreEqual("BABAC",c.Encrypt("BABAC"));
+        }
+        [TestMethod]
+        public void EncryptPositiveOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = 1;
+            Assert.AreEqual("CBCBA", c.Encrypt("BABAC"));
+        }
+
+        [TestMethod]
+        public void EncryptNegativeOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = -1;
+            Assert.AreEqual("ACACB", c.Encrypt("BABAC"));
+        }
+
+        [TestMethod]
+        public void DecryptZeroOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = 0;
+            Assert.AreEqual("ACACB", c.Decrypt("ACACB"));
+        }
+        [TestMethod]
+        public void DecryptPositiveOffset()// works in console test with 1... why failing test?
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = 1;
+            Assert.AreEqual("CBCBD", c.Decrypt("BABAC")); 
+        }
+
+        [TestMethod]
+        public void DecryptNegativeOffset()
+        {
+            Cipher c = new Cipher("ABC");
+            c.offset = -1;
+            Assert.AreEqual("BABAC", c.Decrypt("ACACB")); 
+        }
+    }
+}
